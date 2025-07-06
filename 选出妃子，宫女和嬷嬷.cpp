9393066b4ebf4713a5f1cd859c2131d11d2e2eb3
Louisyoung <3095746912@qq.com>
2025-07-06 17:35:14 +0800

    添加了选出妃子，宫女和嬷嬷

---
+++ 选出妃子，宫女和嬷嬷.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct st_girl {//超女结构体
+    int age;
+    int height;
+    string sc;//身材
+    bool yz;//颜值
+}stgirl;
+
+void func(const st_girl &stgirl);
+
+int main()
+{
+    //妃子:1)年龄18-25岁;2)身高165-178cm;3)身材火辣;4)颜值漂亮。
+    //宫女:1)年龄18-30岁;2)身高160-165cm;3)身材火辣或者普通;4)颜值漂亮。
+    //嬷嬷:1)年龄35-40岁;2)身高155-165cm;3)身材普通或者飞机场;4颜值一般。
+    stgirl = {19,178,"身材火辣",true};
+    st_girl & ststgirl = stgirl;
+    func(ststgirl);
+    return 0;
+}
+
+void func(const st_girl &stgirl) {
+    int age = stgirl.age;
+    int height = stgirl.height;
+    string sc = stgirl.sc;
+    bool yz = stgirl.yz;
+    if (age >= 18 && age <= 25 && height >= 165 && height <= 178 && sc == "身材火辣" && yz == true) {
+        cout << "妃子" << endl;
+    }
+    else if (age >= 18 && age <= 30 && height >= 160 && height <= 165 && sc == "身材火辣或者普通" && yz == true) {
+        cout << "宫女" << endl;
+    }
+    else if (age >= 35 && age <= 40 && height >= 155 && height <= 165 && sc == "身材普通或者飞机场" && yz == false) {
+        cout << "嬷嬷" << endl;
+    }
+}
