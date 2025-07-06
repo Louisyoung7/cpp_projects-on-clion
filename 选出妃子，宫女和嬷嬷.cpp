c5960c5857485b7ce33a9ace012b92566e6b4c65
Louisyoung <3095746912@qq.com>
2025-07-06 17:54:57 +0800

    修改了一下‘选出妃子，宫女和嬷嬷’

--- 选出妃子，宫女和嬷嬷.cpp
+++ 选出妃子，宫女和嬷嬷.cpp
@@ -15,7 +15,15 @@ int main()
     //妃子:1)年龄18-25岁;2)身高165-178cm;3)身材火辣;4)颜值漂亮。
     //宫女:1)年龄18-30岁;2)身高160-165cm;3)身材火辣或者普通;4)颜值漂亮。
     //嬷嬷:1)年龄35-40岁;2)身高155-165cm;3)身材普通或者飞机场;4颜值一般。
-    stgirl = {19,178,"身材火辣",true};
+    cout << "请输入年龄：" << endl;
+    cin >> stgirl.age;
+    cout <<"请输入身高：" << endl;
+    cin >> stgirl.height;
+    cout << "请输入身材：" << endl;
+    cin >> stgirl.sc;
+    cout << "请输入颜值：" << endl;
+    cin >> stgirl.yz;
+    stgirl = {stgirl.age,stgirl.height,stgirl.sc,stgirl.yz};
     st_girl & ststgirl = stgirl;
     func(ststgirl);
     return 0;
@@ -26,13 +34,13 @@ void func(const st_girl &stgirl) {
     int height = stgirl.height;
     string sc = stgirl.sc;
     bool yz = stgirl.yz;
-    if (age >= 18 && age <= 25 && height >= 165 && height <= 178 && sc == "身材火辣" && yz == true) {
+    if (age >= 18 && age <= 25 && height >= 165 && height <= 178 && sc == "火辣" && yz == true) {
         cout << "妃子" << endl;
     }
-    else if (age >= 18 && age <= 30 && height >= 160 && height <= 165 && sc == "身材火辣或者普通" && yz == true) {
+    else if (age >= 18 && age <= 30 && height >= 160 && height <= 165 && (sc == "火辣" || sc == "普通") && yz == true) {
         cout << "宫女" << endl;
     }
-    else if (age >= 35 && age <= 40 && height >= 155 && height <= 165 && sc == "身材普通或者飞机场" && yz == false) {
+    else if (age >= 35 && age <= 40 && height >= 155 && height <= 165 && (sc == "普通" || sc == "飞机场") && yz == false) {
         cout << "嬷嬷" << endl;
     }
 }
